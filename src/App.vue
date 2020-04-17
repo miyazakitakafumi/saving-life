e3ff55a564b783779205d2c8beca27a1bd774ed9
宮崎貴史 <>
2020-04-17 16:53:14 +0900

    add simple card

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,56 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <v-list dense>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-action>
+          <v-list-item-content @click="move('/')">
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon>mdi-contact-mail</v-icon>
+          </v-list-item-action>
+          <v-list-item-content @click="move('/about')">
+            <v-list-item-title>About</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app color="indigo" dark>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
     <v-content>
-        <router-link to="/">Home</router-link>
-        <router-link to="/about">About</router-link>
-        <router-view/>
+      <v-container class="fill-height" fluid>
+        <router-view />
+      </v-container>
     </v-content>
 
+    <v-footer color="indigo" app>
+      <span class="white--text">&copy; 2019</span>
+    </v-footer>
   </v-app>
 </template>
 
 <script>
-
 export default {
   name: 'App',
-
   components: {
     //
   },
-
   data: () => ({
     //
-  })
+  }),
+  methods: {
+    move: function(path){
+      this.$router.push(path)
+    }
+  }
 }
 </script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,40 @@
 <template>
-  <div>
-    Main Page
-  </div>
+  <v-row>
+    <v-col>
+      <v-card
+              class="mx-auto"
+              max-width="344"
+              outlined
+      >
+        <v-list-item three-line>
+          <v-list-item-content>
+            <div class="overline mb-4">OVERLINE</div>
+            <v-list-item-title class="headline mb-1">Headline 5</v-list-item-title>
+            <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-avatar
+                  tile
+                  size="80"
+                  color="grey"
+          ></v-list-item-avatar>
+        </v-list-item>
+
+        <v-card-actions>
+          <v-btn text>Button</v-btn>
+          <v-btn text>Button</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-col>
+    <v-col>bbb</v-col>
+    <v-col>ccc</v-col>
+    <v-col>ddd</v-col>
+  </v-row>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  components: {}
+  components: {},
 }
 </script>
